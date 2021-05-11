ef8fa2903aa14aacd10b36eb56bc73b0306a6375
broky123 <83728051+broky123@users.noreply.github.com>
2021-05-11 09:29:30 +0700

    Create start.py

--- handlers/start.py
+++ handlers/start.py
@@ -6,8 +6,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""<b>👋🏻 Haii Guys, Apa Kabar Kamu??:)) {message.from_user.first_name}!</b>
-
+        f"""<b>Haii.. {message.from_user.first_name} Aku adalah 𓊈one-heart music𓊉\n
 𝘈𝘒𝘜 𝘈𝘋𝘈𝘓𝘈𝘏 𝘉𝘖𝘛 𝘔𝘜𝘚𝘐𝘒 𝘛𝘌𝘓𝘌𝘎𝘙𝘈𝘔 𝘠𝘈𝘕𝘎 𝘋𝘐 𝘒𝘌𝘔𝘉𝘈𝘕𝘎𝘒𝘈𝘕 𝘖𝘓𝘌𝘏 : @boyfriendnice
 
 𝘈𝘗𝘈𝘉𝘐𝘓𝘈 𝘐𝘕𝘎𝘐𝘕 𝘔𝘌𝘕𝘎𝘎𝘜𝘕𝘈𝘒𝘈𝘕 𝘈𝘒𝘜 𝘐𝘕𝘝𝘐𝘛𝘌 𝘈𝘒𝘜 𝘋𝘈𝘕 𝘈𝘚𝘐𝘚𝘚𝘛𝘈𝘕𝘛 𝘕𝘠𝘈 𝘓𝘈𝘓𝘜 𝘑𝘈𝘋𝘐𝘒𝘈𝘕 𝘈𝘋𝘔𝘐𝘕 𝘒𝘌𝘋𝘜𝘈𝘕𝘠𝘈 𝘈𝘎𝘈𝘙 𝘉𝘐𝘚𝘈 𝘉𝘌𝘙𝘑𝘈𝘓𝘈𝘕 𝘋𝘌𝘕𝘎𝘈𝘕 𝘓𝘈𝘕𝘊𝘈𝘙.
@@ -18,12 +17,11 @@ async def start(_, message: Message):
     𝙹𝙸𝙺𝙰 𝚃𝙸𝙳𝙰𝙺, 𝙰𝚂𝙸𝚂𝚃𝙴𝙽 𝚃𝙸𝙳𝙰𝙺 𝙰𝙺𝙰𝙽 𝙽𝙰𝙸𝙺.
 ┗━━━━━━━━━━━━━━
 🤵𝓒𝓻𝓮𝓪𝓽𝓮𝓭 𝓫𝔂 : [loveMe](https://t.me/boyfriendnice)
-☘️𝓣𝓱𝓪𝓷𝓴𝓼 𝓯𝓸𝓻 : [Group Support](https://t.me/remaja_virtual62)
+☘️𝓣𝓱𝓪𝓷𝓴𝓼 𝓯𝓸𝓻 : [Grup Support](https://t.me/remaja_virtual62)
 ━━━━━━━━━━━━━━
 𝐁𝐎𝐓 𝐌𝐔𝐒𝐈𝐊 : @officialheartbot - 𝐀𝐒𝐈𝐒𝐒𝐓𝐀𝐍𝐓 𝐌𝐔𝐒𝐈𝐊 : @AsisstantOneHeart
-
-
-        """,
+ 
+</b>""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
@@ -37,7 +35,7 @@ async def start(_, message: Message):
                         "Group", url="https://t.me/remaja_virtual62") 
                   ],[
                     InlineKeyboardButton(
-                        "My bot help", url="https://t.me/@Asisstant_groupbot"
+                        "My Bot Help!", url="https://t.me/naylaanggitabot"
                     )
                 ]
             ]
@@ -45,39 +43,26 @@ async def start(_, message: Message):
      disable_web_page_preview=True
     )
 
-@Client.on_message(filters.command("reload") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**💕 Pemutar Musik Is The On!**""",
+      await message.reply_text("""**Aku sudah online, ayo kita joget ceria! 🎶**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "Group Support", url="https://t.me/remaja_virtual62"
-                    ),
                     InlineKeyboardButton(
                         "Owner", url="https://t.me/boyfriendnice"
                     )
-                ]
-            ]
-        )
-   )
-
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
-async def gstart(_, message: Message):
-      await message.reply_text("""**🎧 Pemutar Musik Is The On!**""",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Onwer", url="https://t.me/boyfriendnice") 
                 ],[
                     InlineKeyboardButton(
-                        "Group Support", url="https://t.me/remaja_virtual62"
+                        "✅ Yes!", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "❌ No!", callback_data="close"
                     )
                 ]
             ]
         )
-   )
+    )
 
 @Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
@@ -86,7 +71,8 @@ async def gstart(_, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                   
+                    InlineKeyboardButton(
+                        "🦇 Cara Memakai Bot Music!", url="https://t.me/humangabutguys/91577"
                     )
                 ]
             ]
